f10950a9c45d91efd314e83f65cb5166ed81e9be
NguyenHuyVinh <vinhnh.uet.195@gmail.com>
2016-12-03 14:50:39 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+   <title>Home</title>
+  <link rel="stylesheet" type="text/css" href="public/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="public/css/font-awesome.min.css">
+  <link rel="stylesheet" href="public/css/styleHome.css">
+  <link rel="stylesheet" href="public/css/owl.carousel.css">
+  <link rel="stylesheet" href="public/css/login.css">
+ 
+ 
+</head>
+
+<body>
+
+<header>
+	<nav class="navbar navbar-default affix" role="navigation">
+	  <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#"></a>
+      </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
+	        <ul class="nav navbar-nav">
+	            <li><a href="home">Trang chủ</a></li>
+	            <li><a href="#">Giới thiệu</a></li>
+	            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Khoa              
+                <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Công nghệ thông tin</a></li>
+                  <li><a href="#">Điện tử Viễn thông </a></li>
+                  <li><a href="#">Vật lý Kỹ thuật & Công nghệ Nanô</a></li>
+                  <li><a href="#">Cơ học Kỹ thuật & Tự động hóa</a></li>
+                </ul>
+              </li>         
+              <li><a href="giangvien">Giảng viên</a></li>
+	            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Bộ môn              
+                <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Các hệ thống thông tin</a></li>
+                  <li><a href="#">Công nghệ Phần mềm </a></li>
+                  <li><a href="#">Khoa học Máy tính</a></li>
+                  <li><a href="#">Mạng và Truyền thông Máy tính</a></li>
+                  <li><a href="#">Khoa học và Kỹ thuật tính toán</a></li>
+                </ul>
+              </li>
+	            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Phòng Thí nghiệm             
+                <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Phòng Thí nghiệm An toàn thông tin</a></li>
+                  <li><a href="#">Phòng Thí nghiệm Công nghệ Tri thức</a></li>
+                  <li><a href="#">Phòng Thí nghiệm Hệ thống Nhúng</a></li>
+                  <li><a href="#">Phòng Thí nghiệm Tương tác Người – Máy</a></li>
+               </ul>
+              </li>
+	            <li><a href="#">Các lĩnh vực,đề tài</a></li>
+	            	        	
+	        </ul>
+	        
+	         <!-- </div> -->
+	        <ul class="nav navbar-nav navbar-right">
+	          <li><a href="#" onclick="document.getElementById('idlogin').style.display='block';
+	                  document.getElementById('idmodal').style.display='block'">Đăng nhập</a></li>
+	            </ul>
+	        <!-- form login register-->
+	        <div id="idmodal" class="modal1">
+	      <form id="idlogin" class="modal1-content animate" style="display: none;">
+	        <div class="title-login">Đăng Nhập</div>
+	        <input class="input-login" type="text" name="uname" placeholder="Mã sinh viên " required>
+	        <input class="input-login" type="password" name="psw" placeholder="Mật khẩu" required>
+	        <button class="btn-login">Đăng Nhập</button>
+	      </form>
+	      </div> <!-- end login -->
+
+
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+</header> <!-- end header-->
+
+
+
+ <!-- content-->
+<div class="container">
+
+</div>
+<!-- End content -->
+	 	
+ 	
+
+<footer class="footer">
+  <div class="footer-wrapper">
+    <div class="footer-widgets">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="widget">
+              <h4 class="widget-title">THÔNG TIN VỀ ĐẠI HỌC CÔNG NGHỆ</h4>
+              <div class="widget-content">
+                <p>Một trong những trường đầu về CNTT</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-4">
+            <div class="widget">
+              <h4 class="widget-title">HỖ TRỢ</h4>
+              <div class="widget-content">
+                <p>Địa chỉ: Số 144 - Xuân Thủy - Cầu Giấy - Hà nội</p>
+                <p>SĐT: 096 470 05 02</p>
+                <p>website: uet.vnu.edu.vn</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-4">
+            <div class="widget">
+              <h4 class="widget-title">FANPAGE</h4>
+              <ul class="list-socials">
+                <li><a href="#" title=""><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#" title=""><i class="fa fa-google-plus"></i></a></li>
+                <li><a href="#" title=""><i class="fa fa-pinterest-p"></i></a></li>
+              </ul>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div> <!-- /.footer-widget -->
+
+  <div class="footer-copyright">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-10 copyright">
+          <span>Copyright &copy; uet.vnu.edu.vn - All Rights Reserved.</span>
+        </div>
+
+        <div class="col-sm-2 language dropup">
+          <span>Language: </span>
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">VN <span class="caret"></span></a>
+          <ul class="dropdown-menu drop-menu">
+            <li><a href="#">English</a></li>
+            <li><a href="#">Vietnamese</a></li>
+            <li><a href="#">Japaniese</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div> <!-- /.footer-copyright -->
+      
+</div> <!-- /.footer-wrapper -->
+    
+</footer> <!-- /footer -->
+ 
+<script type="text/javascript" src="public/js/jquery-1.11.3.min.js"></script>
+<script type="text/javascript" src="public/bootstrap/js/bootstrap.min.js"></script>
+<script src="public/js/owl.carousel.min.js"></script>
+  
+</body>
+</html>
+
+
+
